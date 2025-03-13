7d654f9fc4c8af7b72e7696e2232b8c7552af1f6
nokibsarkar <nokibsarkar@gmail.com>
2025-03-14 05:19:18 +0600

    convert them to an api call

--- provider/session/action.ts
+++ provider/session/action.ts
@@ -4,7 +4,7 @@ import { fetchFromBackend } from "@/server";
 import { redirect } from "next/navigation";
 
 const tokenUri = 'https://meta.wikimedia.org/w/rest.php/oauth2/authorize'
-export const loginInitiateAction = async (base: string, pathName: string | null) => {
+export const loginInitiateActionClient = async (base: string, pathName: string | null) => {
     const clientId = process.env.NEXT_AUTH_CLIENT_ID;
     if (!clientId) {
         throw new Error('Missing NEXT_AUTH_CLIENT_ID')
@@ -17,9 +17,23 @@ export const loginInitiateAction = async (base: string, pathName: string | null)
     }).toString()
     return redirect(`${tokenUri}?${qs}`)
 }
+export const loginInitiateAction = async (base: string, pathName: string | null) => {
+    const qs = `?next=${pathName || '/'}`
+    const res = await fetchFromBackend('/api/v2/user/login' + qs, {
+        cache: 'no-cache',
+        credentials: 'include',
+        redirect: 'manual',
+    });
+    console.log(res)
+    const location = res.headers.get('Location')
+    if (!location) {
+        throw new Error('Location header missing')
+    }
+    return redirect(location)
+}
 export const loginCallbackAction = async (code: string, state: string) => {
     const qs = `?code=${code}&state=${state}`
-    const res = await fetchFromBackend('/user/callback' + qs, {
+    const res = await fetchFromBackend('/api/v2/user/callback' + qs, {
         cache: 'no-cache',
         credentials: 'include',
         redirect: 'manual',
