9778709d2c7dc7b4af50c38da1e6e803bbebf0be
nokibsarkar <nokibsarkar@gmail.com>
2025-03-17 18:42:37 +0600

    fixed loading

--- components/LoadingPopup.tsx
+++ components/LoadingPopup.tsx
@@ -1,9 +1,8 @@
 import LottieWrapper from "./LottieWrapper";
 
-const LoadingPopup = ({ src = '/lottie/loading.lottie', message = 'Loading' }: { src?: string, message?: string }) => (
-    <div className="loading-popup fixed top-0 left-0 w-full h-full bg-transparent z-50 flex items-center justify-center flex-col backdrop-blur-[10px]">
+const LoadingPopup = ({ src = '/lottie/loading.lottie' }: { src?: string, message?: string }) => (
+    <div className="loading-popup fixed top-0 left-0 w-full h-full bg-transparent z-50 flex items-center justify-center flex-col backdrop-blur-[2px]">
         <LottieWrapper src={src} />
-        {message && <p>{message}</p>}
     </div>
 )
 export default LoadingPopup;
