8c8ec45a90da741e8316bb5809bc5af619c63197
nokibsarkar <nokibsarkar@gmail.com>
2025-04-03 20:47:43 +0600

    removed lottie file from nt-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,27 @@
 "use client";
 import { Paper, Typography } from "@mui/material";
-import LottieWrapper from "@/components/LottieWrapper";
+import ErrorIcon from '@/public/molumen-red-square-error-warning-icon.svg';
+import Image from "next/image";
 export default function NotFound() {
-    return <Paper sx={{ padding: 2, textAlign: 'center', color: 'error', my: 2, height: '80%', position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }
-    }>,
-        <LottieWrapper src='/lottie/not-found.lottie' />
+    return <Paper sx={{ padding: 2, textAlign: 'center', color: 'error', my: 2, height: '80%', position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', pt: 5 }
+    }>
+        <Image
+            src={ErrorIcon.src}
+            alt="Error"
+            width={100}
+            height={100}
+            priority
+            style={{
+                width: "100px",
+                height: "auto",
+                margin: "auto",
+                position: "relative",
+                marginTop: '20px',
+                // top: "50%",
+                // left: "50%",
+                // transform: "translate(-50%, -50%)",
+            }}
+        />
         <Typography variant="h2">Not Found</Typography>
         <Typography variant="body1">
             The Page you were looking for was not found.
