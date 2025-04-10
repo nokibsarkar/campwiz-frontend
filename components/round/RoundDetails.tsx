809527defe5736246b36ed1a97f35e47487f9428
Nazmul Haque Naqib <39087050+nokibsarkar@users.noreply.github.com>
2025-04-11 00:32:48 +0600

    Added evaluation type (#142)

--- components/round/RoundDetails.tsx
+++ components/round/RoundDetails.tsx
@@ -1,5 +1,5 @@
 
-import type { Round } from "@/types/round"
+import { EvaluationType, Round } from "@/types/round"
 import Box from "@mui/material/Box";
 
 import Deadline from "./Deadline";
@@ -10,7 +10,22 @@ import JuryList from "./Jury";
 import CircularProgressWithLabel from "./CircularProgressWithLabel";
 import { useMemo } from "react";
 import { RoleWithUsername } from "@/types/role";
-
+import LikeDislike from "@mui/icons-material/ThumbUpOffAlt";
+import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
+import StarsIcon from '@mui/icons-material/Stars';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+const roundTypeIcon = (type: EvaluationType) => {
+    switch (type) {
+        case EvaluationType.BINARY:
+            return <Description description='Yes/No' label="Type" Icon={LikeDislike} />
+        case EvaluationType.RANKING:
+            return <Description description='Ranking' label="Type" Icon={MilitaryTechIcon} />
+        case EvaluationType.SCORE:
+            return <Description description='Ranting' label="Type" Icon={StarsIcon} />
+        default:
+            return <Description description='Unknown, contact us' label="Type" Icon={QuestionMarkIcon} />
+    }
+}
 
 const RoundDetails = ({ round: c }: { round: Round }) => {
     const [juryList,] = useMemo(() => {
@@ -56,6 +71,7 @@ const RoundDetails = ({ round: c }: { round: Round }) => {
                 <CircularProgressWithLabel value={progress} size='10em' color='success' />
                 <div>
                     <Deadline deadline={c.endDate} />
+                    {roundTypeIcon(c.type)}
                     <Description description={c.description} />
                     <Description description={`${c.totalEvaluatedSubmissions} evaluated out of ${c.totalSubmissions}`} label='Submissions' Icon={HistoryEduIcon} />
                     {!c.isPublicJury && <Description description={`${c.totalEvaluatedAssignments} evaluated out of ${c.totalAssignments}`} label='Assignments' Icon={HistoryEduIcon} />}
