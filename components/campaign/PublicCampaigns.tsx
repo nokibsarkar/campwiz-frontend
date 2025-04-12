b055995d7eb7193263bba27dbdc50ee358ee9f04
nokibsarkar <nokibsarkar@gmail.com>
2025-04-12 16:12:14 +0600

    public descending order

--- components/campaign/PublicCampaigns.tsx
+++ components/campaign/PublicCampaigns.tsx
@@ -11,20 +11,22 @@ type PublicRunningCampaignProps = {
     limit: number
 }
 const PublicRunningCampaigns = ({ limit }: PublicRunningCampaignProps) => {
-    const qs = new URLSearchParams({ limit: String(limit), isClosed: 'false', isHidden: 'false' }).toString()
+    const qs = new URLSearchParams({ limit: String(limit), isClosed: 'false', isHidden: 'false', 'sortOrder': 'desc' }).toString()
     const { data: publicCampaignResponse, error, isLoading } = useSWR('/campaign/?' + qs.toString(), fetchAPIFromBackendSingleWithErrorHandling<Campaign[]>);
+
     if (isLoading) return <Skeleton variant="rectangular" width='100%' height={200} sx={{ backgroundColor: 'rgba(0,0,0,0.1)' }} />
     if (error) return <p>Error : {error.message}</p>
     if (!publicCampaignResponse)
         return null;
     if ('detail' in publicCampaignResponse)
         return <p>Error : {error}</p>
-    return publicCampaignResponse.data.length > 0 ? <div style={{
+    const campaigns = publicCampaignResponse.data.toSorted((a, b) => a.campaignId.localeCompare(b.campaignId));
+    return campaigns.length > 0 ? <div style={{
         display: 'flex', flexDirection: 'row', justifyContent: 'center', flexWrap: 'wrap',
         backgroundImage: `url(${PerCampaignBackground.src})`,
         backgroundSize: 'contain',
     }} className="justify-self-auto px-2">
-        {publicCampaignResponse.data.map(v => <SingleCampaignChip campaign={v} key={v.campaignId} />)}
+        {campaigns.map(v => <SingleCampaignChip campaign={v} key={v.campaignId} />)}
     </div>
         : <div className="justify-self-auto p-1 mb-2 block text-center">
             <h3 className="text-2xl font-bold">No Running Campaigns</h3>
