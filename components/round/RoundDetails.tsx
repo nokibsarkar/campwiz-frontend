5c6a1b02c6340762d3c7083b7006f6b611ac9cfa
nokibsarkar <nokibsarkar@gmail.com>
2025-04-15 01:53:19 +0600

    moved it to client side

--- components/round/RoundDetails.tsx
+++ components/round/RoundDetails.tsx
@@ -1,5 +1,5 @@
 
-import { EvaluationType, Round, SubmissionResultSummary } from "@/types/round"
+import { EvaluationType, Round } from "@/types/round"
 import Box from "@mui/material/Box";
 
 import Deadline from "./Deadline";
@@ -7,19 +7,13 @@ import Description from "./Description";
 import Quorum from "./Quorum";
 import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
 import JuryList from "./Jury";
-import CircularProgressWithLabel from "./CircularProgressWithLabel";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import { RoleWithUsername } from "@/types/role";
 import LikeDislike from "@mui/icons-material/ThumbUpOffAlt";
 import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
 import StarsIcon from '@mui/icons-material/Stars';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
-import SummaryIcon from '@mui/icons-material/Assignment';
-import { Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
-import fetchRoundResults from "@/app/campaign/[campaignId]/round/import/round/fetchRoundSummary";
-import useSWR from "swr";
-import { ResponseList } from "@/types";
-import CloseIcon from '@mui/icons-material/Close';
+import RoundSummary from "./RoundSummary";
 const roundTypeIcon = (type: EvaluationType) => {
     switch (type) {
         case EvaluationType.BINARY:
@@ -32,47 +26,7 @@ const roundTypeIcon = (type: EvaluationType) => {
             return <Description description='Unknown, contact us' label="Type" Icon={QuestionMarkIcon} />
     }
 }
-const RoundSummaryDialog = ({ round: c, onClose }: { round: Round, onClose: () => void }) => {
-    const { data: summaryResponse, isLoading, error } = useSWR(`/round/${c.roundId}/results/summary`, fetchRoundResults, {
-        revalidateOnFocus: true,
-        revalidateOnReconnect: true,
-        revalidateOnMount: true,
-    })
-    return <Dialog open onClose={onClose} fullWidth>
-        <DialogTitle>
-            Round Summary
-        </DialogTitle>
 
-        <DialogContent>
-            {isLoading && <CircularProgress size={24} />}
-            {error && <p className="text-red-500">Error: {error.message}</p>}
-            {summaryResponse && !isLoading && !error && (
-                ('details' in summaryResponse) ? <p className="text-red-500">Error: {summaryResponse.details as string}</p> :
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Average Score</TableCell>
-                                <TableCell>Submission Count</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {(summaryResponse as ResponseList<SubmissionResultSummary & { cumulative: number; }>).data.map((row: { averageScore: number, submissionCount: number }, index: number) => (
-                                <TableRow key={index}>
-                                    <TableCell>{row.averageScore}</TableCell>
-                                    <TableCell>{row.submissionCount}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-            )}
-        </DialogContent>
-        <DialogActions>
-            <Button onClick={onClose} variant="outlined" color="error" disabled={isLoading} startIcon={<CloseIcon />}>
-                Close Dialog
-            </Button>
-        </DialogActions>
-    </Dialog>
-}
 const RoundDetails = ({ round: c }: { round: Round }) => {
     const [juryList,] = useMemo(() => {
         if (!c.jury || !c.roles) {
@@ -93,9 +47,6 @@ const RoundDetails = ({ round: c }: { round: Round }) => {
         return [juryList, totalAssigned, totalEvaluated]
 
     }, [c])
-    const [showSummaryOpen, setShowSummaryOpen] = useState(false)
-    const { totalSubmissions, totalEvaluatedSubmissions } = c
-    const progress = totalSubmissions > 0 ? Math.floor(totalEvaluatedSubmissions / totalSubmissions * 100) : 0;
     return (
         <Box sx={{
             textAlign: 'left',
@@ -114,15 +65,7 @@ const RoundDetails = ({ round: c }: { round: Round }) => {
                 alignItems: 'center',
                 justifyContent: 'space-between'
             }}>
-                <div className="round-progress flex flex-col items-center">
-                    <CircularProgressWithLabel value={progress} size='10em' color='success' /><br />
-                    <Button variant="contained"
-                        onClick={() => setShowSummaryOpen(true)}
-                        color="success" size="large" sx={{ borderRadius: 2, px: 2, my: 'auto' }} startIcon={<SummaryIcon />}>
-                        Summary
-                    </Button>
-                    {showSummaryOpen && <RoundSummaryDialog round={c} onClose={() => setShowSummaryOpen(false)} />}
-                </div>
+                <RoundSummary c={c} />
                 <div>
                     <Deadline deadline={c.endDate} />
                     {roundTypeIcon(c.type)}
---
+++ components/round/RoundSummary.tsx
@@ -0,0 +1,67 @@
+"use client"
+import { Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import fetchRoundResults from "@/app/campaign/[campaignId]/round/import/round/fetchRoundSummary";
+import useSWR from "swr";
+import { ResponseList } from "@/types";
+import CloseIcon from '@mui/icons-material/Close';
+import CircularProgressWithLabel from "./CircularProgressWithLabel";
+import { useState } from "react";
+import { Round, SubmissionResultSummary } from "@/types/round";
+import SummaryIcon from '@mui/icons-material/Assessment';
+
+const RoundSummaryDialog = ({ round: c, onClose }: { round: Round, onClose: () => void }) => {
+    const { data: summaryResponse, isLoading, error } = useSWR(`/round/${c.roundId}/results/summary`, fetchRoundResults, {
+        revalidateOnFocus: true,
+        revalidateOnReconnect: true,
+        revalidateOnMount: true,
+    })
+    return <Dialog open onClose={onClose} fullWidth>
+        <DialogTitle>
+            Round Summary
+        </DialogTitle>
+
+        <DialogContent>
+            {isLoading && <CircularProgress size={24} />}
+            {error && <p className="text-red-500">Error: {error.message}</p>}
+            {summaryResponse && !isLoading && !error && (
+                ('details' in summaryResponse) ? <p className="text-red-500">Error: {summaryResponse.details as string}</p> :
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Average Score</TableCell>
+                                <TableCell>Submission Count</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {(summaryResponse as ResponseList<SubmissionResultSummary & { cumulative: number; }>).data.map((row: { averageScore: number, submissionCount: number }, index: number) => (
+                                <TableRow key={index}>
+                                    <TableCell>{row.averageScore}</TableCell>
+                                    <TableCell>{row.submissionCount}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+            )}
+        </DialogContent>
+        <DialogActions>
+            <Button onClick={onClose} variant="outlined" color="error" disabled={isLoading} startIcon={<CloseIcon />}>
+                Close Dialog
+            </Button>
+        </DialogActions>
+    </Dialog>
+}
+const RoundSummary = ({ c }: { c: Round }) => {
+    const [showSummaryOpen, setShowSummaryOpen] = useState(false);
+    const { totalSubmissions, totalEvaluatedSubmissions } = c
+    const progress = totalSubmissions > 0 ? Math.floor(totalEvaluatedSubmissions / totalSubmissions * 100) : 0;
+    return <div className="round-progress flex flex-col items-center">
+        <CircularProgressWithLabel value={progress} size='10em' color='success' /><br />
+        <Button variant="contained"
+            onClick={() => setShowSummaryOpen(true)}
+            color="success" size="large" sx={{ borderRadius: 2, px: 2, my: 'auto' }} startIcon={<SummaryIcon />}>
+            Summary
+        </Button>
+        {showSummaryOpen && <RoundSummaryDialog round={c} onClose={() => setShowSummaryOpen(false)} />}
+    </div>
+}
+export default RoundSummary;
