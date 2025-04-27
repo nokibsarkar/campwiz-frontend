98ec9359e4ff73d0946df2a9d066eb25cdc69e07
nokibsarkar <nokibsarkar@gmail.com>
2025-04-27 20:40:50 +0600

    Added server header

--- middlewares/authorization.ts
+++ middlewares/authorization.ts
@@ -1,21 +1,33 @@
 import fetchSession from "@/server/session";
 import { NextRequest, NextResponse } from "next/server";
 // import LanguageDetectorMiddleware from "./languageDetector";
-
+const serverId = `${process.env.FRONTEND_SERVER_ID}:${process.env.PORT}`;
 const AuthorizationMiddleWare = async (req: NextRequest) => {
     // await LanguageDetectorMiddleware(req);
     if (req.nextUrl.pathname.match(/\/$/))
-        return NextResponse.next()
+        return NextResponse.next({
+            headers: {
+                'X-Frontend-Server-Id': serverId,
+            }
+        })
     if (!req.nextUrl.pathname.startsWith('/user/login')) {
         const session = await fetchSession();
         if (session === null) {
             const url = new URL('/user/login', req.nextUrl)
             url.searchParams.set('next', req.nextUrl.pathname)
-            return NextResponse.redirect(url)
+            return NextResponse.redirect(url, {
+                headers: {
+                    'X-Frontend-Server-Id': serverId,
+                }
+            })
         }
 
     }
-    return NextResponse.next()
+    return NextResponse.next({
+        headers: {
+            'X-Frontend-Server-Id': serverId,
+        }
+    })
 }
 export const AuthorizationConfig = {
     matcher: [
