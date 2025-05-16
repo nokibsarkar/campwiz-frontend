bbbbd573396b5e6363090eb20018fbfe9fa965e1
nokibsarkar <39087050+nokibsarkar@users.noreply.github.com>
2025-05-16 20:01:58 +0000

    Build: ()

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/home/runner/work/campwiz-frontend/campwiz-frontend/app/layout":["static/media/47cbc4e2adbc5db9-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
