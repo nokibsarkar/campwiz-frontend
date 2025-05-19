e2773acfa87fb9168b70dc5e1983e27fcf00a5df
nokibsarkar <nokibsarkar@gmail.com>
2025-05-19 20:15:27 +0600

    Added ml

--- i18n/settings.ts
+++ i18n/settings.ts
@@ -1,5 +1,5 @@
 export const fallbackLng = 'en'
-export const languages = [fallbackLng, 'bn', 'hi', 'mr', 'ta', 'kn']
+export const languages = [fallbackLng, 'bn', 'hi', 'mr', 'ta', 'kn', 'ml']
 export const defaultNS = 'translation'
 export const cookieName = 'i18next'
 
